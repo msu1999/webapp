76bb4ba56152f8f239bad5b01ae55407fbc0ea1a
msu1999 <m_s_unuvar@hotmail.com>
2022-11-26 00:08:18 +0300

    ffff

--- db.js
+++ db.js
@@ -1,43 +1,84 @@
-var sql = require('mssql')
+const { Connection, Request } = require("tedious");
 
- 
-var webconfig = {
-    server: "serverbau.database.windows.net",
-    database:"db",
-    user:"adminsalih",
-    password:"Qwerty123."
+// Create connection to database
+const config = {
+  authentication: {
+    options: {
+      userName: "salihadmin", // update me
+      password: "Qwerty123." // update me
+    },
+    type: "default"
+  },
+  server: "serverbau.database.windows.net", // update me
+  options: {
+    database: "db", //update me
+    encrypt: true
+  }
 };
- const AZURE_CONN_STRING = process.env["Server=serverbau.database.windows.net,1433;Database=db;User Id=salihadmin;Password=Qwerty123.;Encrypt=true"];
 
- 
- 
-function getBlog(){
-     
-    var conn = new sql.ConnectionPool(webconfig);
-    var req = new sql.Request(conn);
-     
-    conn.connect(function(error){
-         
-        if(error){
-             
-            console.log(error);
-            return;
+/* 
+    //Use Azure VM Managed Identity to connect to the SQL database
+    const config = {
+        server: process.env["db_server"],
+        authentication: {
+            type: 'azure-active-directory-msi-vm',
+        },
+        options: {
+            database: process.env["db_database"],
+            encrypt: true,
+            port: 1433
         }
-         
-        req.query("SELECT * FROM db",function(error, r){
-            if(error)
-            {
-                               console.log(error)
-  
-            }
-            else{
-                console.log(r)
-            }
-             
-        })
-         
-    })
-     
+    };
+
+    //Use Azure App Service Managed Identity to connect to the SQL database
+    const config = {
+        server: process.env["db_server"],
+        authentication: {
+            type: 'azure-active-directory-msi-app-service',
+        },
+        options: {
+            database: process.env["db_database"],
+            encrypt: true,
+            port: 1433
+        }
+    };
+
+*/
+
+const connection = new Connection(config);
+
+// Attempt to connect and execute queries if connection goes through
+connection.on("connect", err => {
+  if (err) {
+    console.error(err.message);
+  } else {
+    queryDatabase();
+  }
+  connection.close();
+});
+
+connection.connect();
+
+function queryDatabase() {
+  console.log("Reading rows from the Table...");
+
+  // Read all rows from table
+  const request = new Request(
+    `SELECT * from db`,
+    (err, rowCount) => {
+      if (err) {
+        console.error(err.message);
+      } else {
+        console.log(`${rowCount} row(s) returned`);
+      }
+    }
+  );
+
+  request.on("row", columns => {
+    columns.forEach(column => {
+      console.log("%s\t%s", column.metadata.colName, column.value);
+    });
+  });
+
+  connection.execSql(request);
 }
- 
-getBlog();
