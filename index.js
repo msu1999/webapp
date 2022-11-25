143202643c6a6d14a6419ea03d0db141cf618eae
msu1999 <85367971+msu1999@users.noreply.github.com>
2022-11-25 22:06:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,56 @@
+var http = require('http')
 
-var Connection = require('tedious').Connection;
-var Request = require('tedious').Request
-var TYPES = require('tedious').TYPES;
+http.createServer(onRequest).listen(8888);
+console.log('Server has started');
 
-module.exports = function (context, myTimer) {
-
-    var _currentData = {};
-
-    var config = {
-        userName: 'salihadmin',
-        password: 'Qwerty123.',
-        server: 'serverbau.database.windows.net',
-        options: {encrypt: true, database: 'db'}
-    };
-
-    var connection = new Connection(config);
-    connection.on('connect', function(err) {
-        context.log("Connected");
-        getPerformance();
-    });
-
-    function getPerformance() {
-
-        request = new Request("SELECT * FROM db;", function(err) {
-        if (err) {
-            context.log(err);}
-        });
-
-        request.on('row', function(columns) {
-            _currentData.Best = columns[0].value;
-            _currentData.Average = columns[1].value;;
-            context.log(_currentData);
-        });
-
-        request.on('requestCompleted', function () {
-            //
-        });
-        connection.execSql(request);
-    }
+function onRequest(request, response){
+  response.writeHead(200);
+  response.write('Hello Noders');
 
 
-    context.done();
-};
+  var Connection = require('tedious').Connection;
+  var Request = require('tedious').Request
+  var TYPES = require('tedious').TYPES;
+  
+  module.exports = function (context, myTimer) {
+      var _currentData = {};
+  
+      var config = {
+          userName: 'salihadmin',
+          password: 'Qwerty123.',
+          server: 'serverbau.database.windows.net',
+          options: {encrypt: true, database: 'db'}
+      };
+  
+      var connection = new Connection(config);
+      connection.on('connect', function(err) {
+          context.log("Connected");
+          getPerformance();
+      });
+  
+      function getData() {
+  
+          request = new Request("SELECT * FROM db;", function(err) {
+          if (err) {
+              context.log(err);}
+          });
+  
+          request.on('row', function(columns) {
+              _currentData.Best = columns[0].value;
+              _currentData.Average = columns[1].value;;
+              context.log(_currentData);
+          });
+  
+          request.on('requestCompleted', function () {
+              //
+          });
+          connection.execSql(request);
+      }
+  
+  
+      context.done();
+  };
+
+  response.end();
+}
+
