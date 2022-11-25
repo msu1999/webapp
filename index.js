7df1c9a48cc91c3d9b570a353e738c38a9f3338e
msu1999 <m_s_unuvar@hotmail.com>
2022-11-25 23:33:11 +0300

    asdf

--- index.js
+++ index.js
@@ -2,87 +2,25 @@ var http = require('http');
 http.createServer(onRequest).listen(8888);
 console.log('Server has started');
     
-var Connection = require('tedious').Connection;  
-console.log('tedious');
 
-    var config = {  
-        server: 'serverbau.database.windows.net',
-        authentication: {
-            type: 'default',
-            options: {
-              userName: 'salihadmin',
-             password: 'Qwerty123.',
-            }
-        },
-        options: {
-            // If you are on Microsoft Azure, you need encryption:
-            encrypt: true,
-            port: 1433,
-                    integratedSecurity: true,
-        trustServerCertificate: true,
-            database: 'db',
-            trustServerCertificate: false,
+    const sql = require('mssql')
 
-        }
-    };  
+async () => {
+    try {
+        // make sure that any items are correctly URL encoded in the connection string
+        await sql.connect('Server=serverbau.database.windows.netd,1433;Database=db;User Id=salihadmin;Password=Qwerty123.;Encrypt=true')
+        const result = await sql.query`select * from db`
+        console.dir(result);
+          response.write(result);
+
+    } catch (err) {
+        // ... error checks
+    }
+}
 
 
-
-
-    var connection = new Connection(config); 
-console.log('new cnx \n');
-
-
-
-
-    var Request = require('tedious').Request;  
-    var TYPES = require('tedious').TYPES;  
-
-
-    connection.on('connect', function(err) {  
-        // If no error, then good to proceed.  
-        console.log("Connected");  
-        executeStatement();  
-    });  
-    
-      connection.connect();
-  console.log('connect \n');
-    function executeStatement() {  
-        request = new Request("SELECT * FROM db;", function(err) {  
-        if (err) {  
-            console.log(err);}  
-        });  
-        var result = "";  
-        request.on('row', function(columns) {  
-            columns.forEach(function(column) {  
-              if (column.value === null) {  
-                console.log('NULL');  
-              } else {  
-                result+= column.value + " ";  
-              }  
-            });  
-            console.log(result);  
-            result ="";  
-        });  
-  
-        request.on('done', function(rowCount, more) {  
-        console.log(rowCount + ' rows returned');  
-        });  
-        
-        // Close the connection after the final event emitted by the request, after the callback passes
-        request.on("requestCompleted", function (rowCount, more) {
-            connection.close();
-                response.writeHead(200);
-         response.write('Hello Noders ');
-        });
-
-
-        connection.execSql(request);  
-
-    }  
       
  
-
 
 function onRequest(request, response){
   response.writeHead(200);
