d564be274b7a6c74c1cc6cad2436cda10168d04e
msu1999 <m_s_unuvar@hotmail.com>
2022-11-26 00:20:21 +0300

    ddd

--- db.js
+++ db.js
@@ -1,13 +1,43 @@
-const sql = require('mssql')
+var sql = require('mssql')
 
-async () => {
-    try {
-        // make sure that any items are correctly URL encoded in the connection string
-        await sql.connect('Server=serverbau.database.windows.net,1433;Database=db;User Id=salihadmin;Password=salihadmin.;Encrypt=true')
-        const result = await sql.query`select * from db`
-        console.dir(result)
-        console.log("dddd")
-    } catch (err) {
-        // ... error checks
-    }
+ 
+var webconfig = {
+    server: "serverbau.database.windows.net",
+    database:"db",
+    user:"adminsalih",
+    password:"Qwerty123."
+};
+ const AZURE_CONN_STRING = process.env["Server=serverbau.database.windows.net,1433;Database=db;User Id=salihadmin;Password=Qwerty123.;Encrypt=true"];
+
+ 
+ 
+function getBlog(){
+     
+    var conn = new sql.ConnectionPool(webconfig);
+    var req = new sql.Request(conn);
+     
+    conn.connect(function(error){
+         
+        if(error){
+             
+            console.log(error);
+            return;
+        }
+         
+        req.query("SELECT * FROM db",function(error, r){
+            if(error)
+            {
+                               console.log(error)
+  
+            }
+            else{
+                console.log(r)
+            }
+             
+        })
+         
+    })
+     
 }
+ 
+getBlog();
